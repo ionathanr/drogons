c5f6181d0c6036b10534ea40161f8359dc4d9dfa
Ionathan <ionathan.rus@softvision.ro>
2019-07-25 16:49:36 +0300

    Updates

---
+++ precache-manifest.54d7a3c9dd05cd06a3983d69e490774e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "972cc0cc345e0a5e195e7a1a816529b2",
+    "url": "/drogons/index.html"
+  },
+  {
+    "revision": "9bc75ef44d28b9eded22",
+    "url": "/drogons/static/css/main.cabdc0d2.chunk.css"
+  },
+  {
+    "revision": "baf961a1263ccd6a2d24",
+    "url": "/drogons/static/js/2.0f58faaa.chunk.js"
+  },
+  {
+    "revision": "9bc75ef44d28b9eded22",
+    "url": "/drogons/static/js/main.3174c0ce.chunk.js"
+  },
+  {
+    "revision": "9c7fa3611c75784fdb0a",
+    "url": "/drogons/static/js/runtime~main.88ffe5d0.js"
+  }
+]);
